487499c5edaa4c8353c4acd892a949e447600b6a
liuyuzhe <liuyuzhe110@qq.com>
2017-02-06 21:13:23 +0800

    修改gradle工程配置，引入jsp、jstl、Spring DAO库。spring配置整理

--- src/main/java/com/strongliu/blog/vo/PostVo.java
+++ src/main/java/com/strongliu/blog/vo/PostVo.java
@@ -2,5 +2,10 @@ package com.strongliu.blog.vo;
 
 import com.strongliu.blog.entity.Post;
 
-public class PostVo extends Post {
+public class PostVo {
+	private String id;
+	private String createTime;
+	private String title;
+	private String excerpt;
+	private String content;
 }
