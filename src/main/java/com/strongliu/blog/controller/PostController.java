6dcbf9466a1f114312150f875f3652464ad8b43f
liuyuzhe <liuyuzhe110@qq.com>
2017-02-08 23:32:30 +0800

    将WebContent内容移动到webapp里，以解决idea 404错误。 基线版本提交

--- src/main/java/com/strongliu/blog/controller/PostController.java
+++ src/main/java/com/strongliu/blog/controller/PostController.java
@@ -9,22 +9,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.strongliu.blog.entity.Post;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(value="/{postId}", method=RequestMethod.GET)
 public class PostController {
 
 	@Autowired
 	PostService postService;
-	
+
+
+	@RequestMapping(value="/{postId}", method=RequestMethod.GET)
+	@ResponseBody
 	public String post(@PathVariable String postId, Model model) {
-		Post post = postService.findPostById(postId);
-		if (post == null) {
-			return "404";
-		}
-		
-		model.addAttribute("post", post);
-		
+//		Post post = postService.findPostById(postId);
+//		if (post == null) {
+//			return "404";
+//		}
+//
+//		model.addAttribute("post", post);
+//
 		return "post";
 	}
 
