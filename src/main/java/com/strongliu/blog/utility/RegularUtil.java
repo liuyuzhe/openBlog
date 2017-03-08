0e3657c2975bf07a661aa331ea84f651d5666b98
liuyuzhe <liuyuzhe110@qq.com>
2017-03-08 21:53:19 +0800

    增加后端用户名密码等的正则匹配验证

--- src/main/java/com/strongliu/blog/utility/RegularUtil.java
+++ src/main/java/com/strongliu/blog/utility/RegularUtil.java
@@ -5,12 +5,22 @@ package com.strongliu.blog.utility;
  */
 public class RegularUtil {
 
-    public static final String USER_NAME = "";
+    // 6-18位英文数字下划线
+    public static final String USER_NAME = "^[A-Za-z0-9_-]{6,18}$";
 
-    public static final String PASSWORD = "";
+    // 6-18位英文数字下划线
+    public static final String PASSWORD = "^[A-Za-z0-9_-]{6,18}$";
 
-    public static final String NICK_NAME = "";
+    // 6-18位英文/中文数字下划线
+    public static final String NICK_NAME = "^[A-Za-z0-9_-\\u4E00-\\u9FA5]{2,16}$";
 
-    public static final String EMAIL = "";
+    public static final String EMAIL = "^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+\\.[a-zA-Z0-9_-]+$";
+
+    public static final String MOBILEPHONE = "^1[3|4|5|7|8][0-9]{9}$";
+
+    public static final String TELEPHONE = "^\\d{3}-\\d{8}|\\d{4}-\\d{7,8}$";
+
+    public static final String URL = "^(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?$";
+
 
 }
