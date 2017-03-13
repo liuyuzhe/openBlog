eddc3c071d800f4fd47afa863b768255d7c92bda
liuyuzhe <liuyuzhe110@qq.com>
2017-03-13 23:21:54 +0800

    通过js正则表达式判断注册信息

--- src/main/webapp/static/js/user.js
+++ src/main/webapp/static/js/user.js
@@ -2,14 +2,20 @@
  * Created by liuyuzhe on 2017/3/4.
  */
 
+//document.write("<script src='../../../static/js/regular.js' charset='utf-8' ></script>");
+
 $("#register").click(function() {
-    $.post("/user/register", $("#registerForm").serialize(), function(response, status) {
-        if (response.code == 0) {
-            window.location.href = '/user/login';
-        } else {
-            alert(response.message);
-        }
-    });
+    //var username = $("#registerForm input[name='username']").val();
+    var username = $(this).parents("form").find("input[name='username']").val();
+    $("#alertUsername").toggle(!isUsername(username));
+
+    //$.post("/user/register", $("#registerForm").serialize(), function(response, status) {
+    //    if (response.code == 0) {
+    //        window.location.href = '/user/login';
+    //    } else {
+    //        alert(response.message);
+    //    }
+    //});
 });
 
 $("#login").click(function() {
