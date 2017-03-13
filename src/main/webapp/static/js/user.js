4e405cd59aa895a57f08510ae6324b7fec3909b5
liuyuzhe <liuyuzhe110@qq.com>
2017-03-13 23:49:26 +0800

    完成前端注册页面信息正则验证，尚未完成实时监测

--- src/main/webapp/static/js/user.js
+++ src/main/webapp/static/js/user.js
@@ -5,17 +5,45 @@
 //document.write("<script src='../../../static/js/regular.js' charset='utf-8' ></script>");
 
 $("#register").click(function() {
-    //var username = $("#registerForm input[name='username']").val();
-    var username = $(this).parents("form").find("input[name='username']").val();
-    $("#alertUsername").toggle(!isUsername(username));
+    var username = $("#registerForm input[name='username']").val();
+    if (!isUsername(username)) {
+        $("#alertUsername").show();
+        return;
+    } else {
+        $("#alertUsername").hide();
+    }
 
-    //$.post("/user/register", $("#registerForm").serialize(), function(response, status) {
-    //    if (response.code == 0) {
-    //        window.location.href = '/user/login';
-    //    } else {
-    //        alert(response.message);
-    //    }
-    //});
+    var email = $("#registerForm input[name='email']").val();
+    if (!isEmail(email)) {
+        $("#alertEmail").show();
+        return;
+    } else {
+        $("#alertEmail").hide();
+    }
+
+    var password = $("#registerForm input[name='password']").val();
+    if (!isPassword(password)) {
+        $("#alertPassword").show();
+        return;
+    } else {
+        $("#alertPassword").hide();
+    }
+
+    var repeat = $("#repeatPassword").val();
+    if (repeat != password) {
+        $("#alertRepeatPassword").show();
+        return;
+    } else {
+        $("#alertRepeatPassword").hide();
+    }
+
+    $.post("/user/register", $("#registerForm").serialize(), function(response, status) {
+        if (response.code == 0) {
+            window.location.href = '/user/login';
+        } else {
+            alert(response.message);
+        }
+    });
 });
 
 $("#login").click(function() {
