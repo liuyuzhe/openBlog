feb47683479f88e2b344726b9c702b28b6ff38e6
liuyuzhe <liuyuzhe110@qq.com>
2017-05-04 21:40:31 +0800

    修改数据库中枚举字段为varchar，以防止后续枚举值变化带来的升级不便

--- openBlog.sql
+++ openBlog.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50717
  File Encoding         : utf-8
 
- Date: 05/03/2017 23:30:53 PM
+ Date: 05/04/2017 21:39:05 PM
 */
 
 SET FOREIGN_KEY_CHECKS = 0;
@@ -22,7 +22,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `attach_table`;
 CREATE TABLE `attach_table` (
   `attach_id` int(11) NOT NULL AUTO_INCREMENT,
-  `attach_slug` varchar(20) NOT NULL COMMENT '链接地址',
+  `attach_slug` varchar(100) NOT NULL COMMENT '链接地址',
   `attach_name` varchar(20) NOT NULL COMMENT '名称',
   `attach_type` varchar(20) NOT NULL COMMENT '类型',
   `attach_create_time` datetime NOT NULL COMMENT '创建时间',
@@ -36,8 +36,8 @@ CREATE TABLE `attach_table` (
 -- ----------------------------
 DROP TABLE IF EXISTS `option_table`;
 CREATE TABLE `option_table` (
-  `option_name` varchar(50) NOT NULL COMMENT '名称',
-  `option_value` longtext NOT NULL COMMENT '值',
+  `option_name` varchar(20) NOT NULL COMMENT '名称',
+  `option_value` varchar(255) NOT NULL COMMENT '值',
   PRIMARY KEY (`option_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -54,9 +54,9 @@ CREATE TABLE `post_table` (
   `post_content` longtext NOT NULL COMMENT '内容',
   `post_create_time` datetime NOT NULL COMMENT '创建时间',
   `post_update_time` datetime NOT NULL COMMENT '更新时间',
-  `post_status` enum('publish','draft','private') NOT NULL DEFAULT 'publish' COMMENT '文章状态',
-  `post_comment_status` enum('open','close') NOT NULL DEFAULT 'open' COMMENT '评论状态',
-  `post_type` enum('post','page') NOT NULL DEFAULT 'post' COMMENT '文章类型',
+  `post_status` varchar(20) NOT NULL DEFAULT 'publish' COMMENT '文章状态(''publish'',''draft'',''private'')',
+  `post_comment_status` varchar(20) NOT NULL DEFAULT 'open' COMMENT '评论状态(''open'',''close'')',
+  `post_type` varchar(20) NOT NULL DEFAULT 'post' COMMENT '文章类型(''post'',''page'')',
   `post_comment_count` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '评论数',
   `post_read_count` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '阅读数',
   `post_spot_count` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '点赞数',
@@ -85,9 +85,9 @@ CREATE TABLE `relationship_table` (
 DROP TABLE IF EXISTS `term_table`;
 CREATE TABLE `term_table` (
   `term_id` int(11) NOT NULL AUTO_INCREMENT,
-  `term_slug` varchar(20) NOT NULL COMMENT '链接地址',
+  `term_slug` varchar(100) NOT NULL COMMENT '链接地址',
   `term_name` varchar(20) NOT NULL COMMENT '名称',
-  `term_type` enum('category','tag') NOT NULL COMMENT '类型',
+  `term_type` varchar(20) NOT NULL COMMENT '类型(''category'',''tag'')',
   `term_count` int(10) unsigned NOT NULL COMMENT '所属内容数',
   PRIMARY KEY (`term_id`),
   UNIQUE KEY `term_slug` (`term_slug`) USING BTREE
@@ -99,11 +99,11 @@ CREATE TABLE `term_table` (
 DROP TABLE IF EXISTS `user_table`;
 CREATE TABLE `user_table` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(60) NOT NULL COMMENT '用户名',
-  `user_password` varchar(255) NOT NULL COMMENT '密码',
+  `user_name` varchar(50) NOT NULL COMMENT '用户名',
+  `user_password` varchar(50) NOT NULL COMMENT '密码',
   `user_nickname` varchar(50) NOT NULL COMMENT '昵称',
   `user_email` varchar(50) NOT NULL COMMENT '邮箱',
-  `user_avatars_url` varchar(100) DEFAULT NULL COMMENT '头像URL',
+  `user_avatars_url` varchar(100) NOT NULL COMMENT '头像URL',
   `user_register_time` datetime NOT NULL COMMENT '注册时间',
   `user_activate_time` datetime NOT NULL COMMENT '活跃时间',
   PRIMARY KEY (`user_id`),
